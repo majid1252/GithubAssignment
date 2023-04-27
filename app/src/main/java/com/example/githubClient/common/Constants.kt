16abfbb3a14b0a3848577dbeaefc0cb4d904af72
majid1252 <majid.jafari.iut@gmail.com>
2023-04-28 00:04:09 +0800

    Setup Glide module with proper cache manager

--- app/src/main/java/com/example/githubClient/common/Constants.kt
+++ app/src/main/java/com/example/githubClient/common/Constants.kt
@@ -1,5 +1,13 @@
 package com.example.githubClient.common
 
+/**
+ * Constants used throughout the app.
+ */
 object Constants {
+    // Endpoints
     const val GITHUB_BASE_URL = "https://api.github.com/"
+
+    // Glide values
+    const val GLIDE_CACHE_SIZE = 1024 * 1024 * 100 // 100 MB
+    const val GLIDE_CACHE_DIR_NAME = "glide_disk_cache"
 }
